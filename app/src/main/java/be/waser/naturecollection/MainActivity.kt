bed9c14daf9d685e427b4ad542c91f4ca4d9d1be
walid.serokh <walid.serokh@student.vinci.be>
2021-01-27 23:05:24 +0100

    inject fragment container

--- app/src/main/java/be/waser/naturecollection/MainActivity.kt
+++ app/src/main/java/be/waser/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package be.waser.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import be.waser.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injecter le fragment dans notre boite (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
