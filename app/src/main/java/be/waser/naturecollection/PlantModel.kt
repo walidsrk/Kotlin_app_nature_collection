fab6ea7d5cbb7a98c4766ea38e688fee29c8a908
walid.serokh <walid.serokh@student.vinci.be>
2021-01-29 23:27:57 +0100

    add PlantModel class

---
+++ app/src/main/java/be/waser/naturecollection/PlantModel.kt
@@ -0,0 +1,8 @@
+package be.waser.naturecollection
+
+class PlantModel (
+    val name:String = "Tulipe",
+    val description:String = "Petite description",
+    val imageUrl:String = "http://graven.yt/plant.jpg",
+    val liked:Boolean = false
+)
